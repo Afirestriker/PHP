9453cea92c55396ec032e2776236daa8494dad1d
Ashish Agrawal <89931316+Afirestriker@users.noreply.github.com>
2022-02-17 13:55:17 +0530

    Add files via upload

---
+++ Practical Programs/Session_Management/Cookies/Emp_cookies.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Collect Employee Details</title>
+
+	<style type="text/css">
+		
+		hr{border:  1px solid red}
+		h1{text:align: center; color: red}
+		body{background-color: yellow}
+
+	</style>
+
+</head>
+<body>
+
+	<h1>Employee Information</h1>
+	<hr>
+
+	<form action="Emp_Cookies2.php" method="post">
+
+		Enter Name: <input type="text" name="name"><br>
+		Enter Address: <input type="text" name="address"><br>
+		Enter Mobile No. <input type="text" name="mobileno"><br>
+
+		<input type="submit"><br>
+	</form>
+
+</body>
+</html>
